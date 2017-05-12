c32a800cd2df0f90cd810377e6fe12a87ef268d5
Spencer Alan <spencer.alan@icloud.com>
2017-05-12 10:15:54 -0700

    add call back to capitalize shoe and store names

--- lib/store.rb
+++ lib/store.rb
@@ -2,4 +2,13 @@ class Store < ActiveRecord::Base
   has_many :store_shoes
   has_many :shoes, through: :store_shoes
   validates :name, length: { maximum: 100 }
+
+  before_save :capitalize_title
+
+  private
+
+  def capitalize_title
+    self.name = name.split.map(&:capitalize).join(" ")
+  end
+
 end
--- spec/shoe_store_spec.rb
+++ spec/shoe_store_spec.rb
@@ -3,11 +3,13 @@ require "spec_helper"
 describe Shoe do
   it { should have_many :stores }
   it { should_not allow_value("blahblah" * 20).for(:name) }
+  it { is_expected.to callback(:capitalize_title).before(:save) }
 end
 
 describe Store do
   it { should have_many :shoes }
   it { should_not allow_value("blahblah" * 20).for(:name) }
+  it { is_expected.to callback(:capitalize_title).before(:save) }
 end
 
 describe StoreShoe do
